343ba28cdc64d911a86c43029f87b3437834c5be
marc-pages-onclick <mpages@on-click.es>
2020-11-06 07:14:40 +0100

    Add testing of results

--- tests/unit/components/search.spec.ts
+++ tests/unit/components/search.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 import Search from '@/components/Search.vue';
+import axios from "axios";
 
 describe('Search.vue', () => {
 
@@ -9,6 +10,35 @@ describe('Search.vue', () => {
       propsData: { label },
     });
     expect(wrapper.find('.search-label').text()).toMatch(label);
+  });
+
+  it('check if results are showing', () => {
+    const searchQuery = 'hello';
+    const expectedResults = [
+      {
+        normalized_name: 'hello',
+        name: 'hello'
+      },
+      {
+        normalized_name: 'hello',
+        name: 'bye'
+      },
+    ];
+
+    const getResultsFnc = (query: string) => {
+      return new Promise( (resolve, reject) => {
+        resolve(expectedResults);
+      })
+    };
+
+    const wrapper = shallowMount(Search, {
+      propsData: { getResultsFnc },
+    });
+
+    wrapper.find('.input-container input').setValue(searchQuery);
+
+    expect(wrapper.find('.results').text()).toMatch(/hello/)
+    expect(wrapper.find('.results').text()).toMatch(/bye/)
   });
 
   // TODO: Add test to check if it returns results
